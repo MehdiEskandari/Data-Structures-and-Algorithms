cba28968b09fd908d16d6836c932e52903fc0a90
Mehdi Eskandari <idevaq@gmail.com>
2022-09-20 18:06:35 +0430

    draft

--- exercises/linkedLink-easy.js
+++ exercises/linkedLink-easy.js
@@ -8,54 +8,15 @@ class Node {
 class LinkedList {
   constructor() {
     this.head = null;
-    this.length = 0;
+    this.size = 0;
   }
 
-  insertHead(element) {
-    const node = new Node(element, this.head);
-    this.length++;
-    this.head = node;
-  }
+  isEmpty = () => this.size === 0;
 
-  removeHead() {}
-
-  indexAt() {}
-
-  insertAt() {}
-
-  removeAt() {}
-
-  clear() {}
-
-  #isEmpty() {
-    return this.length === 0;
-  }
-
-extractData (node) {
-    container = ""
-
-    if(node.next){
-        container
-    }
-}
-
-  print() {
-    let container = "";
-
-    for (let index = 0; index < this.length; index++) {
-      container += `${this.head.next} --> `;
-    }
-
-    return container
-  }
-
-  #checkIndexValidation() {}
+  getSize = () => this.size;
 }
 
 const linkedlist = new LinkedList();
 
-linkedlist.insertHead(10);
-linkedlist.insertHead(100);
-linkedlist.insertHead(1000);
-
-console.log(linkedlist.print());
+console.log("isEmpty: ", linkedlist.isEmpty());
+console.log("size: ", linkedlist.getSize());
